e4d8c5063c63c1e38fee4af075697ef496bcf4a6
Florian Cargoët <florian.cargoet@gmail.com>
2016-04-23 15:34:04 +0200

    pencil filter

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -25,6 +25,14 @@ class Workspace extends React.Component {
     const { width, height, zoomFactor } = this.state;
     return (
       <svg height="100%" width="100%">
+        <defs>
+          <filter id="pencil" filterUnits="userSpaceOnUse" x="0" y="0" height="100%" width="100%">
+            {/*<feTurbulence baseFrequency="0.9" numOctaves="3" type="fractalNoise"></feTurbulence>*/}
+            {/*<feDisplacementMap scale="3" xChannelSelector="R" in="SourceGraphic"></feDisplacementMap>*/}
+            <feTurbulence baseFrequency="0.1" numOctaves="3" type="fractalNoise" />
+            <feDisplacementMap  scale="6"  xChannelSelector="R" in="SourceGraphic" />
+          </filter>
+        </defs>
         <Zoomable speed={0.2} handle=".background" onZoom={this.onZoom}>
           <ComponentsContainer width={width} height={height} zoomFactor={zoomFactor} />
           <CustomDragLayer  zoomFactor={zoomFactor} />
