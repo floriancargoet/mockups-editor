44a2ada029954caa583b85fcdd218b879ef17c4b
Florian Cargoët <florian.cargoet@gmail.com>
2016-04-30 19:39:26 +0200

    font awesome icons

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
-import { DropdownButton, MenuItem, Button } from 'react-bootstrap';
+import { DropdownButton, MenuItem, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import * as components from '../mockup-components';
 
-const buttonStyle = {
+const toolbarStyle = {
   margin: 5
 };
 
+function SR({ children }) { // eslint-disable-line react/prop-types
+  return (<span className="sr-only">{children}</span>);
+}
 
+function Icon({ name, title }) { // eslint-disable-line react/prop-types
+  return (<i className={'fa fa-' + name} title={title} ariaHidden></i>);
+}
 
 const Toolbar = ({ onButtonClicked, selection, selectionContainsGroup }) => {
   return (
-    <div className="toolbar">
-      <Button style={buttonStyle} onClick={() => onButtonClicked('undo')}>Undo</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('redo')}>Redo</Button>
-      <DropdownButton title="Add component">
+    <ButtonToolbar style={toolbarStyle}>
+
+      <ButtonGroup>
+        <Button onClick={() => onButtonClicked('undo')}>
+          <Icon name="undo" title="Undo" />
+          <SR>Undo</SR>
+        </Button>
+        <Button onClick={() => onButtonClicked('redo')}>
+          <Icon name="repeat" title="Redo" />
+          <SR>Redo</SR>
+        </Button>
+      </ButtonGroup>
+
+      <DropdownButton title={<span><Icon name="plus" /> Add component</span>}>
         {
           Object.keys(components).map(componentName => (
             <MenuItem onClick={() => onButtonClicked('create', components[componentName])}>{componentName}</MenuItem>
           ))
         }
       </DropdownButton>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('duplicate')}>Duplicate</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('delete')} disabled={(selection.length === 0)}>Delete</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('group')} disabled={selection.length < 2}>Group</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('ungroup')} disabled={!selectionContainsGroup}>Ungroup</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('bring-to-top')} disabled={(selection.length === 0)}>Bring To Top</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('bring-up')} disabled={(selection.length === 0)}>Bring Up</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('push-down')} disabled={(selection.length === 0)}>Push Down</Button>
-      <Button style={buttonStyle} onClick={() => onButtonClicked('push-to-bottom')} disabled={(selection.length === 0)}>Push To Bottom</Button>
-    </div>
+
+      <Button onClick={() => onButtonClicked('duplicate')}><Icon name="clone" /> Duplicate</Button>
+      <Button onClick={() => onButtonClicked('delete')} disabled={(selection.length === 0)} bsStyle="danger"><Icon name="trash-o" /> Delete</Button>
+
+      <ButtonGroup>
+        <Button onClick={() => onButtonClicked('group')} disabled={selection.length < 2}><Icon name="object-group" /> Group</Button>
+        <Button onClick={() => onButtonClicked('ungroup')} disabled={!selectionContainsGroup}><Icon name="object-ungroup" /> Ungroup</Button>
+      </ButtonGroup>
+
+      <ButtonGroup>
+        <Button onClick={() => onButtonClicked('bring-to-top')} disabled={(selection.length === 0)}>Bring To Top</Button>
+        <Button onClick={() => onButtonClicked('bring-up')} disabled={(selection.length === 0)}>Bring Up</Button>
+        <Button onClick={() => onButtonClicked('push-down')} disabled={(selection.length === 0)}>Push Down</Button>
+        <Button onClick={() => onButtonClicked('push-to-bottom')} disabled={(selection.length === 0)}>Push To Bottom</Button>
+      </ButtonGroup>
+
+    </ButtonToolbar>
   );
 };
 
