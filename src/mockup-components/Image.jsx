7c81939774034338ac16328cd22468f775904be8
Florian Cargoët <florian.cargoet@gmail.com>
2016-05-01 11:21:36 +0200

    added Image mockup component

---
+++ src/mockup-components/Image.jsx
@@ -0,0 +1,62 @@
+import React, { PropTypes, Component } from 'react';
+import random from 'random-number-in-range';
+
+class Image extends Component {
+
+  render() {
+    const { width, height, properties } = this.props;
+    const rectRadius = Number(properties.radius) || 0;
+
+    const style = {
+      strokeWidth: properties.strokeWidth,
+      stroke: 'black',
+      fill: 'none'
+    };
+    const m = properties.strokeWidth / 2;
+    return (
+      <svg height={height} width={width}>
+        <defs>
+          <clipPath id="clip">
+          <rect x={0 + m} y={0 + m} rx={rectRadius} width={width - 2 * m} height={height - 2 * m}/>
+          </clipPath>
+        </defs>
+
+        <rect x={0 + m} y={0 + m} rx={rectRadius} width={width - 2 * m} height={height - 2 * m} style={style} />
+        <g clipPath="url(#clip)">
+          <line x1={0} y1={0} x2={width} y2={height} style={style}/>
+          <line x1={width} y1={0} x2={0} y2={height} style={style}/>
+        </g>
+      </svg>
+    );
+  }
+
+}
+
+Image.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  properties: PropTypes.object
+};
+
+Image.editors = {
+  strokeWidth: 'Number',
+  radius: 'Number'
+};
+
+Image.create = (config = {}) => ({
+  type: 'Image',
+  x: random(50, 500),
+  y: random(50, 500),
+  width: random(50, 200),
+  height: random(50, 200),
+  locked: false,
+  ...config,
+
+  properties: {
+    strokeWidth: 2,
+    radius: 0,
+    ...config.properties
+  }
+});
+
+export default Image;
--- src/mockup-components/index.js
+++ src/mockup-components/index.js
@@ -1,6 +1,7 @@
 export Box from './Box.jsx';
 export Button from './Button.jsx';
 export ButtonBar from './ButtonBar.jsx';
+export Image from './Image.jsx';
 export Path from './Path.jsx';
 export Text from './Text.jsx';
 export Title from './Title.jsx';
